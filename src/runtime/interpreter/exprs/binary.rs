f84ff0c172a8d2ac500959dc415007e07321be3f
jonah.fu <ferock@gmail.com>
2026-03-08 01:09:59 +0800

    feat(interpreter): 添加一元和二元运算表达式求值功能
    - 实现了二元运算表达式的求值逻辑，支持比较运算符和基本运算符
    - 实现了一元运算表达式的求值逻辑，支持取反和逻辑非操作
    - 集成了到解释器中的eval_binary和eval_unary方法
    - 支持多种二元运算符包括相等性、比较、逻辑运算等
    - 支持一元负号和逻辑非运算符的执行处理

---
+++ src/runtime/interpreter/exprs/binary.rs
@@ -0,0 +1,30 @@
+//! 二元运算表达式求值
+
+use crate::ast::{BinaryOp, Expr};
+use crate::runtime::{RuntimeError, Value, ValueCompare, ValueOps};
+
+use super::super::Interpreter;
+
+impl Interpreter {
+    /// 执行二元运算
+    pub fn eval_binary(&mut self, left: &Expr, op: BinaryOp, right: &Expr) -> Result<Value, RuntimeError> {
+        let left_val = self.eval_expr(left)?;
+        let right_val = self.eval_expr(right)?;
+        
+        match op {
+            // 比较运算符
+            BinaryOp::Eq
+            | BinaryOp::Ne
+            | BinaryOp::Lt
+            | BinaryOp::Le
+            | BinaryOp::Gt
+            | BinaryOp::Ge
+            | BinaryOp::And
+            | BinaryOp::Or
+            | BinaryOp::Xor
+            | BinaryOp::Is => Ok(left_val.compare(op, &right_val)),
+            // 其他运算符
+            _ => left_val.binary_op(op, &right_val),
+        }
+    }
+}
---
+++ src/runtime/interpreter/exprs/unary.rs
@@ -0,0 +1,18 @@
+//! 一元运算表达式求值
+
+use crate::ast::{Expr, UnaryOp};
+use crate::runtime::{Value, ValueConversion};
+
+use super::super::Interpreter;
+
+impl Interpreter {
+    /// 执行一元运算
+    pub fn eval_unary(&mut self, op: UnaryOp, operand: &Expr) -> Result<Value, crate::runtime::RuntimeError> {
+        let val = self.eval_expr(operand)?;
+        
+        match op {
+            UnaryOp::Neg => Ok(Value::Number(-val.to_number())),
+            UnaryOp::Not => Ok(Value::Boolean(!val.is_truthy())),
+        }
+    }
+}
